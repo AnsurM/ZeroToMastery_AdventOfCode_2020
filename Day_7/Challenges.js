5d8bea73b33154b4d7e1626e3d0781ee9dc714c9
AnsurM <ansurmehdi@gmail.com>
2020-12-08 03:37:47 +0500

    challenge 1 completed

--- Day_7/Challenges.js
+++ Day_7/Challenges.js
@@ -15,14 +15,38 @@ const getParentBags = (bagsList, bagsToLookFor) => {
     return parentBags;
 }
 
-let bagsToFind = ["shiny gold"];
 let totalBagsWithGold = [];
-
-let bagsToLookFor = getParentBags(bagsRuleSet, bagsToFind);
-
+let bagsToLookFor = getParentBags(bagsRuleSet, ["shiny gold"]);
 while (bagsToLookFor.length) {
     totalBagsWithGold =  [...new Set([...totalBagsWithGold, ...bagsToLookFor])];
     bagsToLookFor = getParentBags(bagsRuleSet, bagsToLookFor);
 }
 
 console.log("Bags with Shiny Gold Inside: ", totalBagsWithGold.length);
+
+// challenge 2
+
+// let totalBags = 0;
+// const getInnerBags = (bag) => {
+//     if(!bag) return;
+//     const [parent, inner] = bag.split(" contain ");
+//     if(!inner) return;
+//     const innerBags = inner.split(", ");
+//     if(!innerBags) return;
+//     console.log(parent, innerBags, totalBags);
+//     innerBags.forEach((innerBag) => {
+//         const bagQuantity = Number(innerBag.split(" ")[0]);
+//         const bagStringIndex = innerBag.indexOf(" bag");
+//         const bagColor = innerBag.slice(2, bagStringIndex);
+//         if(!bagQuantity || !bagColor) return console.log("No other bags found in: ", parent);
+//         totalBags += bagQuantity;
+//         setTimeout(() => {
+//             getInnerBags(getBagInfo(bagColor));            
+//         }, 3000);
+//     })
+// }
+
+// const getBagInfo = bagColor => bagsRuleSet.find(bag => bag.includes(`${bagColor} bags contain`));
+
+// getInnerBags(getBagInfo("shiny gold"));
+// console.log(`Bags Inside Gold: `, totalBags);
